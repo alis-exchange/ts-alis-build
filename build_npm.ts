4ddba07431ac657f36bf7c85b7eeb3d7cbf589c0
newton@alisx.com <newton@alisx.com>
2024-10-15 13:54:02 +0300

    chore(): Add github publish workflow

--- build_npm.ts
+++ build_npm.ts
@@ -2,6 +2,13 @@ import { build, emptyDir } from "https://deno.land/x/dnt@0.37.0/mod.ts";
 
 await emptyDir("./npm");
 
+const version = Deno.args[0];
+
+if (!version) {
+  console.error("Version argument is required");
+  Deno.exit(1);
+}
+
 await build({
   entryPoints: ["./mod.ts"],
   outDir: "./npm",
@@ -10,7 +17,7 @@ await build({
   },
   package: {
     name: "@alis-build/utils",
-    version: Deno.args[0],
+    version: version,
     description: "A collection of common utilities used at Alis Exchange.",
     license: "APACHE-2.0",
     repository: {
